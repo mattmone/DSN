033828432cf9bb0c011f40e3f09d80dcb8146ba3
Matthew Mone <a.matthew.mone@dawswbvs1002>
2020-01-23 12:01:55 -0700

    beginning attempts at google apis for youtube stream

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,32 @@ import { buildTitles } from './titles.js';
 import { audioTracks } from './tracks.js';
 
 
+// google oauth client id: 979387771633-6sq071rlbc9u53fkam01ap82fj4v0rfj.apps.googleusercontent.com
+// google oauth client secret: nsr-pOlAJ4XNvFIUTOCFCJsG
+
+// function start() {
+//   // Initializes the client with the API key and the Translate API.
+//   gapi.client.init({
+//     'apiKey': 'AIzaSyCvV0RM0aIpXIWgP7luE6C6-2rydq0iSoU',
+//     'discoveryDocs': ['https://www.googleapis.com/youtube/v3/liveBroadcasts'],
+//   }).then(function() {
+//     // Executes an API request, and returns a Promise.
+//     // The method name `language.translations.list` comes from the API discovery.
+//     return gapi.client.language.translations.list({
+//       q: 'hello world',
+//       source: 'en',
+//       target: 'de',
+//     });
+//   }).then(function(response) {
+//     console.log(response.result.data.translations[0].translatedText);
+//   }, function(reason) {
+//     console.log('Error: ' + reason.result.error.message);
+//   });
+// };
+
+// Loads the JavaScript client library and invokes `start` afterwards.
+gapi.load('client', start);
+
 let gameState = {
   player1: {
     score: 0
